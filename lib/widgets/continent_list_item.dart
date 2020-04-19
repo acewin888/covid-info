6288e16ff809f1f35c6034dc8fbcd8dfb6eca7a6
Kevin Sun <kevinsun8803@gmail.com>
2020-04-19 02:07:11 -0400

    add card

--- lib/widgets/continent_list_item.dart
+++ lib/widgets/continent_list_item.dart
@@ -42,99 +42,103 @@ class ContinentItem extends StatelessWidget {
 
     if (continent == null) return Text('NULL', style: localTheme.body1);
 
-    return Table(
-      columnWidths: const {
-        0: IntrinsicColumnWidth(),
-        1: FlexColumnWidth(1.0),
-      },
-      children: [
-        TableRow(
-          children: [
-            Text(
-              'Update Time:',
-              style: boldStyle,
-            ),
-            Text(
-              formatTime(continent.updateTime),
-              style: localTheme.body1,
-            ),
-          ],
-        ),
-        TableRow(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 8.0),
-              child: Text('Continent:', style: boldStyle),
-            ),
-            Text(
-              continent.name?.toString() ?? 'NULL',
-              style: localTheme.body1,
-            ),
-          ],
-        ),
-        TableRow(
-          children: [
-            Text('Total cases:', style: boldStyle),
-            Text(
-              formatNumber(continent.totalCases),
-              style: localTheme.body1,
-            ),
-          ],
-        ),
-        TableRow(children: [
-          Text('Today Cases:', style: boldStyle),
+    return Container(
+      padding: EdgeInsets.only(left: 8.0, bottom: 8.0, right: 8.0),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(const Radius.circular(20.0))),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
           Text(
-            formatNumber(continent.todayCases),
-            style: localTheme.body1,
+            continent.name,
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 50.0,
+                fontWeight: FontWeight.bold),
           ),
-        ]),
-        TableRow(
-          children: [
-            Text('Total Deaths:', style: boldStyle),
-            Text(
-              formatNumber(continent.deaths),
-              style: localTheme.body1,
+          Text(
+            "Updated " + formatTime(continent.updateTime),
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 26.0,
+                fontWeight: FontWeight.bold),
+          ),
+          Card(
+            color: Colors.blueGrey,
+            child: Table(
+              columnWidths: const {
+                0: IntrinsicColumnWidth(),
+                1: FlexColumnWidth(1.0),
+              },
+              children: [
+                TableRow(
+                  children: [
+                    Text('Total cases:', style: boldStyle),
+                    Text(
+                      formatNumber(continent.totalCases),
+                      style: localTheme.body1,
+                    ),
+                  ],
+                ),
+                TableRow(children: [
+                  Text('Today Cases:', style: boldStyle),
+                  Text(
+                    formatNumber(continent.todayCases),
+                    style: localTheme.body1,
+                  ),
+                ]),
+                TableRow(
+                  children: [
+                    Text('Total Deaths:', style: boldStyle),
+                    Text(
+                      formatNumber(continent.deaths),
+                      style: localTheme.body1,
+                    ),
+                  ],
+                ),
+                TableRow(
+                  children: [
+                    Text('Recovered:', style: boldStyle),
+                    Text(
+                      formatNumber(continent.recoveredCases),
+                      style: localTheme.body1,
+                    ),
+                  ],
+                ),
+                TableRow(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Text('Active Cases:', style: boldStyle),
+                    ),
+                    Text(
+                      formatNumber(continent.activeCases),
+                      style: localTheme.body1,
+                    ),
+                  ],
+                ),
+                TableRow(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Text('Critial cases:', style: boldStyle),
+                    ),
+                    Text(
+                      formatNumber(continent.criticalCases),
+                      style: localTheme.body1,
+                    ),
+                  ],
+                )
+              ],
             ),
-          ],
-        ),
-        TableRow(
-          children: [
-            Text('Recovered:', style: boldStyle),
-            Text(
-              formatNumber(continent.recoveredCases),
-              style: localTheme.body1,
-            ),
-          ],
-        ),
-        TableRow(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 8.0),
-              child: Text('Active Cases:', style: boldStyle),
-            ),
-            Text(
-              formatNumber(continent.activeCases),
-              style: localTheme.body1,
-            ),
-          ],
-        ),
-        TableRow(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 8.0),
-              child: Text('Critial cases:', style: boldStyle),
-            ),
-            Text(
-              formatNumber(continent.criticalCases),
-              style: localTheme.body1,
-            ),
-          ],
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
 
-String formatNumber(int number){
+String formatNumber(int number) {
   return NumberFormat('#,##0', 'en_us').format(number);
 }
