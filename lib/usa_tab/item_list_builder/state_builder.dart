dd2b5248bef1b005efcb204f08b7db26eb46c1e2
Kevin Sun <kevinsun8803@gmail.com>
2020-04-21 06:48:11 -0400

    add navigation to Detail screen

--- lib/usa_tab/item_list_builder/state_builder.dart
+++ lib/usa_tab/item_list_builder/state_builder.dart
@@ -1,3 +1,4 @@
+import 'package:covidinfo/detail_screen/detail_page.dart';
 import 'package:covidinfo/model/usa_state.dart';
 import 'package:covidinfo/util/utils.dart';
 import 'package:covidinfo/widgets/dynamic_list.dart';
@@ -9,91 +10,102 @@ class StateBuilder extends CustomBuilder<StateInfo> {
     final localTheme = Theme.of(context).textTheme;
     final boldStyle = localTheme.body1.copyWith(fontWeight: FontWeight.w600);
 
-    return Card(
-      margin: const EdgeInsets.all(16.0),
-      color: Colors.blueGrey,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Table(
-            columnWidths: const {
-              0: IntrinsicColumnWidth(),
-              1: FlexColumnWidth(1.0),
-            },
-            children: [
-              TableRow(
-                children: [
-                  Text('State name:', style: boldStyle),
-                  Text(
-                    item.name,
-                    style: localTheme.body1,
-                  ),
-                ],
-              ),
-              TableRow(children: [
-                Text('Total cases:', style: boldStyle),
-                Text(
-                  formatNumber(item.cases),
-                  style: localTheme.body1,
-                ),
-              ]),
-              TableRow(
-                children: [
-                  Text('Today case:', style: boldStyle),
-                  Text(
-                    formatNumber(item.todayCases),
-                    style: localTheme.body1,
-                  ),
-                ],
-              ),
-              TableRow(
-                children: [
-                  Text('Total Deaths:', style: boldStyle),
-                  Text(
-                    formatNumber(item.deaths),
-                    style: localTheme.body1,
-                  ),
-                ],
-              ),
-              TableRow(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(right: 8.0),
-                    child: Text('Today Deaths:', style: boldStyle),
-                  ),
-                  Text(
-                    formatNumber(item.todayDeaths),
-                    style: localTheme.body1,
-                  ),
-                ],
-              ),
-              TableRow(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(right: 8.0),
-                    child: Text('Total Tests done:', style: boldStyle),
-                  ),
-                  Text(
-                    formatNumber(item.tests),
-                    style: localTheme.body1,
-                  ),
-                ],
-              ),
-              TableRow(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(right: 8.0),
-                    child: Text('Test per On Million Population:', style: boldStyle),
-                  ),
-                  Text(
-                    formatNumber(item.testsPerOneMillion),
-                    style: localTheme.body1,
-                  ),
-                ],
-              )
-            ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => USDetailScreen(title: 'Sales Dashboard 2018'),
           ),
-        ],
+        );
+      },
+      child: Card(
+        margin: const EdgeInsets.all(16.0),
+        color: Colors.blueGrey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Table(
+              columnWidths: const {
+                0: IntrinsicColumnWidth(),
+                1: FlexColumnWidth(1.0),
+              },
+              children: [
+                TableRow(
+                  children: [
+                    Text('State name:', style: boldStyle),
+                    Text(
+                      item.name,
+                      style: localTheme.body1,
+                    ),
+                  ],
+                ),
+                TableRow(children: [
+                  Text('Total cases:', style: boldStyle),
+                  Text(
+                    formatNumber(item.cases),
+                    style: localTheme.body1,
+                  ),
+                ]),
+                TableRow(
+                  children: [
+                    Text('Today case:', style: boldStyle),
+                    Text(
+                      formatNumber(item.todayCases),
+                      style: localTheme.body1,
+                    ),
+                  ],
+                ),
+                TableRow(
+                  children: [
+                    Text('Total Deaths:', style: boldStyle),
+                    Text(
+                      formatNumber(item.deaths),
+                      style: localTheme.body1,
+                    ),
+                  ],
+                ),
+                TableRow(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Text('Today Deaths:', style: boldStyle),
+                    ),
+                    Text(
+                      formatNumber(item.todayDeaths),
+                      style: localTheme.body1,
+                    ),
+                  ],
+                ),
+                TableRow(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Text('Total Tests done:', style: boldStyle),
+                    ),
+                    Text(
+                      formatNumber(item.tests),
+                      style: localTheme.body1,
+                    ),
+                  ],
+                ),
+                TableRow(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Text('Test per On Million Population:',
+                          style: boldStyle),
+                    ),
+                    Text(
+                      formatNumber(item.testsPerOneMillion),
+                      style: localTheme.body1,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
