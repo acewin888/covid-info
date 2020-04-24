4a1067f8b31f72873a88ffc7b879b90494b12e0c
Robert Keazor <rkeazor@gmail.com>
2020-04-24 15:05:10 -0400

    -Add dependency  for flutter charts -Add DeathPerDay as a model representation of charts to be created

---
+++ lib/chart/model/DeathPerDay.dart
@@ -0,0 +1,9 @@
+import 'package:charts_flutter/flutter.dart';
+
+class DeathPerDay {
+  final int day;
+  final int numberOfDeaths;
+  final Color color;
+
+  DeathPerDay(this.day, this.numberOfDeaths, this.color);
+}
