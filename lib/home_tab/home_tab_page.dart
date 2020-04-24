78e8b8406d2996a5fd5c8fe5befd7a39d07bffea
Kevin Sun <kevinsun8803@gmail.com>
2020-04-24 16:53:50 -0400

    add loading for refresh

--- lib/home_tab/home_tab_page.dart
+++ lib/home_tab/home_tab_page.dart
@@ -15,6 +15,8 @@ class HomeTab extends StatefulWidget {
 
 class HomeTabState extends State<HomeTab> {
   Future<List<Continent>> futrueContinents;
+  List<Continent> continentList = List();
+  var shouldShowLoading = false;
 
   @override
   void initState() {
@@ -28,12 +30,16 @@ class HomeTabState extends State<HomeTab> {
       // Appbar
       appBar: AppBar(
         // Title
-          title: Text("Continent Info"),
+        title: Text("Continent Info"),
         actions: <Widget>[
           IconButton(
               icon: Icon(Icons.refresh),
               onPressed: () {
                 // TODO refresh
+                setState(() {
+                  shouldShowLoading = true;
+                  _refreshAPICall();
+                });
                 //
               }),
         ],
@@ -44,11 +50,21 @@ class HomeTabState extends State<HomeTab> {
           future: futrueContinents,
           builder: (context, snapshot) {
             if (snapshot.hasData) {
+              if (shouldShowLoading) {
+                return CircularProgressIndicator();
+              }
+
+              var shouldShowList;
+              if(continentList.isEmpty){
+                shouldShowList = snapshot.data;
+              }else{
+                shouldShowList = continentList;
+              }
               return ListView(
                 padding: const EdgeInsets.symmetric(horizontal: 16.0),
                 children: [
                   const SizedBox(height: 16.0),
-                  CustomViewList<Continent>(snapshot.data, ContinentBuilder()),
+                  CustomViewList<Continent>(shouldShowList, ContinentBuilder()),
                   const SizedBox(height: 16.0),
                 ],
               );
@@ -62,5 +78,15 @@ class HomeTabState extends State<HomeTab> {
         ),
       ),
     );
+  }
+
+  _refreshAPICall() {
+    fetchContinents().then((continents) {
+      setState(() {
+        shouldShowLoading = false;
+        continentList.clear();
+        continentList.addAll(continents);
+      });
+    });
   }
 }
